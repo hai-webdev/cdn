ccff4c59725ad2c536b20670966efa11053f3bc7
zb_hai <hai_web@163.com>
2023-08-14 15:38:04 +0800

    初次提交，二维码为测试二维码

---
+++ contact.js
@@ -0,0 +1,28 @@
+﻿// 需要设置的客服微信
+const contactArr = [
+  {
+    name: "yhb",
+    wechatSrc: "./images/wechat/yhb.png",
+  },
+  {
+    name: "stt",
+    wechatSrc: "./images/wechat/stt.png",
+  },
+];
+
+// 随机函数
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// 设置联系方式的函数
+function setContact(contactArr){
+    const wechatObj = contactArr[getRandom(0, contactArr.length)];
+    const wechatDom = document.querySelectorAll(".h-server-wechat");
+    for (let i = 0; i < wechatDom.length; i++) {
+      wechatDom[i].setAttribute("src", wechatObj.wechatSrc);
+    }
+}
+
+
+
